aab595600fdc8c2e4b5d7464fa9441ea49352744
Amanda Iaria <amanda.iaria@gmail.com>
2015-11-03 10:11:01 -0500

    V4

--- 404.php
+++ 404.php
@@ -1,6 +1,5 @@
-<?php get_header();  ?>
+<?php get_header();   ?>
 <article role="main" id="page-404">
-  <h1 class="page-title ta-center">Uh-Oh!
-    <p class="ta-center">The page you're looking for doesn't seem to exist!  Please try again.</p>
-  </h1>
-</article><?php get_footer();  ?>
+  <h1 class="page-title ta-center">Uh-Oh!</h1>
+  <p class="ta-center">The page you're looking for doesn't seem to exist!  Please try </p>
+</article><?php get_footer();  ?> ?>
