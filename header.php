71692f0ed6ef86d99c2c38ced223674abe69b97a
Amanda Iaria <amanda.iaria@gmail.com>
2015-11-19 16:24:10 -0500

    favicon

--- header.php
+++ header.php
@@ -17,7 +17,7 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="alternate" href="http://example.com" hreflang="en-us" />
-		<link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/favicon.ico">
+		<link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/assets/img/favicon.ico">
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>"> 
 		
 		
