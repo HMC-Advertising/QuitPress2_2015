de471b326156636f57fa9c9eb7db197052356296
Amanda Iaria <amanda@wearehmc.com>
2016-01-13 10:33:28 -0500

    fixing the single.php
    Fixing the single page

--- single.php
+++ single.php
@@ -1,8 +1,8 @@
 <?php get_header();  if ( have_posts() ) : while ( have_posts() ) : the_post();  ?>
 <div class="row">
   <main role="main" class="main col-md-12">
-    <div class="main-indent">
-      <h1 class="page-title">:<?php the_title();  ?>
+    <div class="container">
+      <h1 class="page-title"> <?php the_title();  ?>
       </h1><?php the_content();  ?>
     </div>
   </main>
